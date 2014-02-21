a44d55c8790b3127c02357745af4864bed6cd9d5
dunand <jeffnadeau@gmail.com>
2014-02-20 22:43:22 -0500

    critiques

--- bingowallet.py
+++ bingowallet.py
@@ -1,113 +1,114 @@
-#|=========================================================================================
+#|========================================================================
 #|
 #|   bingowallet.py                                           [Python console script]
 #|
 #|       This simple utility to help you generate a super secure Bitcoin Private key using
-#|       anything you have to pick random numbers. A dice, a coin, a bag of marbles. I 
+#|       anything you have to pick random numbers. A dice, a coin, a bag of marbles. I
 #|       writed this script because I wanted to generate a bitcoin private key using bingo
 #|       balls numbered from 1 to 75.
 #|
 #|   Language:   Python version 2.x
-#|                   (Tested with 2.7.5+ & 3.3.2+ on Ubuntu 13.10)       
+#|                   (Tested with 2.7.5+ & 3.3.2+ on Ubuntu 13.10)
 #|
 #|   Licensing:
 #|       GPLv2 (http://opensource.org/licenses/GPL-2.0)
 #|
 #|vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
-import time
+import math
 
-MIN_VALUE = '0000000000000000000000000000000000000000000000000000000000000001'
-MAX_VALUE = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141'
-base64 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/']
-privBin = ''
-privHex = ''
-privB64 = ''
+min_value = '0000000000000000000000000000000000000000000000000000000000000001'
+max_value = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141'
+max_value_int = int(max_value, base=16)
+base_64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+priv_bin = ''
+priv_hex = ''
+priv_base_64 = ''
 
-try: 
-	input = raw_input
-except NameError: 
-	pass
+try:
+    input = raw_input
+except NameError:
+    pass
 
-def isValidNumber(s, minValue, maxValue):
+
+def is_valid_number(s, min_value, max_value):
     try:
         number = int(s)
-        return number >= minValue and number <= maxValue
+        return number >= min_value and number <= max_value
     except ValueError:
         return False
 
-def haveEnoughRandomBits(base, exp):
-	return base**exp >= 2**256
 
-def howManyPickForFullEntropy(base):
-	i = 0
-	while not haveEnoughRandomBits(base, i):
-		i += 1
-	return i
+def input_number(message, min_value, max_value):
+    while True:
+        num_dec = input(message)
+        if is_valid_number(num_dec, min_value, max_value):
+            num_dec = int(num_dec)
+            problem = False
+        else:
+            print("Each character typed must be a numeral " +
+                  str(min_value) + " - " + str(max_value) + ".  Try again.")
+            problem = True
+        if not problem:
+            break
+    return num_dec
 
-def inputNumber(message, minValue, maxValue):
-	while True:
-		numDec = input(message)
-		if isValidNumber(numDec, minValue, maxValue):
-			numDec = int(numDec)
-			problem = False
-		else:    
-			print("Each character typed must be a numeral "+str(minValue)+" - "+str(maxValue)+".  Try again.")
-			problem = True
-		if not problem:
-			break
-	return numDec
-
-print("\nThis program will help you generate a Bitcoin private key using a simple random number generator like a dice, a coin or a bag of bingo balls!")
+print(
+    "\nThis program will help you generate a Bitcoin private key using a simple random number generator like a dice, a coin or a bag of bingo balls!")
 print("\nThe first step is to pick a base.")
 print("If you use a 6 sided dice, choose 6.")
 print("If you use a 12 sided dice, choose 12.")
 print("If you want to use a coin, choose 2.")
 print("If you want to pick bingo balls in a bag you choose 75...")
 
-BASE = inputNumber("Your base: ", 2, 2**256)
-howManyPick = howManyPickForFullEntropy(BASE);
+base = input_number("Your base: ", 2, 2 ** 256)
+how_many_pick = int(math.ceil(math.log(max_value_int, base)))
 
-print("\nNow you need to choose if you start to count at 0 or 1. For example, if you use a dice numbered from 1 to 6, choose 1.")
+print(
+    "\nNow you need to choose if you start to count at 0 or 1. For example, if you use a dice numbered from 1 to 6, choose 1.")
 
-startNumbering = inputNumber("You want to start to count at 0 or 1: ", 0, 1)
-validInputMax = BASE-1+startNumbering
+start_numbering = input_number("You want to start to count at 0 or 1: ", 0, 1)
+valid_input_max = base - 1 + start_numbering
 
 print("\nOK, now it's time for you to start picking numbers randomly")
-print("You need to pick "+str(howManyPick)+" numbers to generate 256 bits of randomness")
-print("Enter each number one by one. Valid number is "+str(startNumbering)+" to "+str(validInputMax)+"\n")
+print("You need to pick " + str(how_many_pick)
+      + " numbers to generate 256 bits of randomness")
+print("Enter each number one by one. Valid number is " +
+      str(start_numbering) + " to " + str(valid_input_max) + "\n")
 
-privDec = 0
-i = 0
-while not haveEnoughRandomBits(BASE, i):
-	numDec = inputNumber("#" + str(i+1) + ": ", startNumbering, validInputMax)-startNumbering
-	privDec += numDec * BASE**i
-	i+=1
+priv_dec = 0
+for i in range(how_many_pick):
+    num_dec = input_number(
+        "#" + str(i + 1) + ": ",
+        start_numbering,
+        valid_input_max) - start_numbering
+    priv_dec += num_dec * base ** i
 
-privBin = "{0:0256b}".format(privDec)[0:256]
+priv_bin = "{0:0256b}".format(priv_dec)[0:256]
 print("\nPrivate key Binary Format:")
-print(privBin)
+print(priv_bin)
 
 for i in range(64):
-	numBin = privBin[i*4:(i+1)*4]
-	numHex = hex(int(numBin, 2))[2:]
-	privHex += numHex.upper()
+    num_bin = priv_bin[i * 4:(i + 1) * 4]
+    num_hex = hex(int(num_bin, 2))[2:]
+    priv_hex += num_hex.upper()
 
-print("\nPrivate Key Hexadecimal Format (64 characters [0-9A-F]):");
-print(privHex)
+print("\nPrivate Key Hexadecimal Format (64 characters [0-9A-F]):")
+print(priv_hex)
 
 for i in range(43):
-	numBin = privBin[i*6:(i+1)*6]
-	if i < 42:
-		numDec = int(numBin, 2)
-	else:
-		numBin = numBin[0:4]
-		numDec = int(numBin+'00', 2)
-	privB64 += base64[numDec]
-privB64 += '='
+    num_bin = priv_bin[i * 6:(i + 1) * 6]
+    if i < 42:
+        num_dec = int(num_bin, 2)
+    else:
+        num_bin = num_bin[0:4]
+        num_dec = int(num_bin + '00', 2)
+    priv_base_64 += base_64[num_dec]
+priv_base_64 += '='
 
-print("\nPrivate key Base64 (44 characters):");
-print(privB64)
+print("\nPrivate key Base64 (44 characters):")
+print(priv_base_64)
 
-if privHex < MIN_VALUE or privHex > MAX_VALUE:
-	print("\nYOU ARE VERY BAD AT PICKING UP RANDOM BALLS. YOUR PRIVATE KEY IS INVALID")
+if priv_hex < min_value or priv_hex > max_value:
+    print(
+        "\nYOU ARE VERY BAD AT PICKING UP RANDOM BALLS. YOUR PRIVATE KEY IS INVALID")
